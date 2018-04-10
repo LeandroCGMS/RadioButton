d41a9f924b54fb52d31b99f50907352e61bcb871
LSC <leandro@localhost.localdomain>
2018-04-10 09:35:17 -0400

    Com erro na referência do inteiro ao número de Id.

---
+++ app/src/main/java/radiobutton/leandro/com/br/radiobutton/MainActivity.java
@@ -0,0 +1,37 @@
+package radiobutton.leandro.com.br.radiobutton;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    private RadioGroup radioGroup;
+    private RadioButton radioButtonEscolhido;
+    private Button botaoEscolher;
+    private TextView textoExibicao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        radioGroup = findViewById(R.id.radioGroupId);
+        botaoEscolher = findViewById(R.id.botaoEscolherId);
+        textoExibicao = findViewById(R.id.textoExibicaoId);
+
+        botaoEscolher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int idRadioButtonEscolhido = radioGroup.getCheckedRadioButtonId();
+                if( idRadioButtonEscolhido > 0){
+                    radioButtonEscolhido = (RadioButton) findViewById(R.id.content(idRadioButtonEscolhido));
+                }
+            }
+        });
+    }
+}
